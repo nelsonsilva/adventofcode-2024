ce6b29ee856ca2fcaea0b4e9f02c5f43fc8efc28
Nelson Silva <nelson@prolaio.com>
2024-12-02 10:17:06 +0000

    Day 1: Historian Hysteria

--- day1.ts
+++ day1.ts
@@ -1,6 +1,28 @@
-import * as log from "@std/log";
+const ascending = (l: number[]) => l.toSorted((a, b) => a - b);
 
-export default () => {
-  log.info('Poor man\'s logging 🙌')
-  return 'Hello World!';
-}
+const zip = (l1: number[], l2: number[]): number[][] => l1.map((a, i) => [a, l2[i]]);
+
+const distances = (l1: number[], l2: number[]) => zip(l1, l2).map(([a, b]) => Math.abs(a - b))
+
+const sum  = (l: number[]) => l.reduce((a, b) => a + b, 0)
+
+const distance = (l1: number[], l2: number[]) => sum(distances(ascending(l1), ascending(l2)))
+
+const occurrences = (n: number, l: number[]) => l.reduce((c, v) => c + (v == n ? 1 : 0), 0)
+
+const similarity = (l1: number[], l2: number[]) => sum(l1.map(v => v * occurrences(v, l2)))
+
+const input = await Deno.readTextFile("day1.txt");
+
+const pairs = input.split('\n').map(l => l.split(/\s+/).map(n => parseInt(n)))
+
+const l = pairs.map(p => p[0])
+const r = pairs.map(p => p[1])
+
+// Part 1
+console.log(distance(l, r))
+
+// Part 2
+console.log(similarity(l, r))
+
+export { distance, similarity }
--- day1_test.ts
+++ day1_test.ts
@@ -1,6 +1,10 @@
 import { assertEquals } from "@std/assert";
-import day1 from "./day1.ts";
+import { distance, similarity } from "./day1.ts";
 
 Deno.test("Day #1", () => {
-  assertEquals(day1(), 'Hello World!');
+  const lists = [[3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]];
+
+  assertEquals(distance(lists[0], lists[1]), 11);
+
+  assertEquals(similarity(lists[0], lists[1]), 31);
 });
