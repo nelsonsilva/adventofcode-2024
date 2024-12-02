b17646f3e5e794a2a606d4bf875ef964aa454543
Nelson Silva <nelson@prolaio.com>
2024-12-02 10:19:36 +0000

    Use subprocess and stdout / stderr

--- server.ts
+++ server.ts
@@ -1,21 +1,24 @@
-import log from './log.ts';
-
-const DAY_ROUTE = new URLPattern({ pathname: '/day/:day'});
+const DAY_ROUTE = new URLPattern({ pathname: "/day/:day" });
 
 async function handler(req: Request): Promise<Response> {
-  const day = DAY_ROUTE.exec(req.url)?.pathname.groups.day
+  const day = DAY_ROUTE.exec(req.url)?.pathname.groups.day;
+  
   if (!day) {
-    return new Response('Not found (try /day/1)', { status: 404 });
+    return new Response("Not found (try /day/1)", { status: 404 });
   }
-  try {
-    const res = (await import(`./day${day}.ts`))?.default()
-    return new Response([...log.messages, '\n', res].join('\n'), { status: 200 });
-  } catch (error) {
-    return new Response((error as Error).message , { status: 500 });
+
+  const command = new Deno.Command("deno", {
+    args: ["run", "--allow-read=.", `./day${day}.ts`],
+  });
+  const { code, stdout, stderr } = await command.output();
+
+  if (code != 0) {
+    return new Response(new TextDecoder().decode(stderr), { status: 500 });
   }
+
+  return new Response(new TextDecoder().decode(stdout), { status: 200 });
 }
 
 if (import.meta.main) {
   Deno.serve(handler);
 }
-
